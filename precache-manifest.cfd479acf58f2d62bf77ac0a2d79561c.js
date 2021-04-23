1de2935b76693f26a14b0d0a07e6bd73d6ba8c9d
luffy1727 <tushig.tushig@gmail.com>
2021-04-23 16:28:46 +0800

    Updates

---
+++ precache-manifest.cfd479acf58f2d62bf77ac0a2d79561c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda91e0111d2a37d76985516b2db1064",
+    "url": "/tushig-listens/index.html"
+  },
+  {
+    "revision": "5340684ea8675fcfc849",
+    "url": "/tushig-listens/static/css/main.2fce297a.chunk.css"
+  },
+  {
+    "revision": "a1f5bb128ce99d5e371e",
+    "url": "/tushig-listens/static/js/2.f4d7aba2.chunk.js"
+  },
+  {
+    "revision": "42884885db5bfef2dff6e4f01ef298a8",
+    "url": "/tushig-listens/static/js/2.f4d7aba2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5340684ea8675fcfc849",
+    "url": "/tushig-listens/static/js/main.486da15d.chunk.js"
+  },
+  {
+    "revision": "f999f692f1d72e9e8649",
+    "url": "/tushig-listens/static/js/runtime-main.ff85a292.js"
+  }
+]);
